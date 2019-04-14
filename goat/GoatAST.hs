09aec668b0663acbedd3fd14cb3d3c006395665a
An (Martin) Luo <aluo1.developer@gmail.com>
2019-04-15 00:59:03 +1000

    add header comment to GoatAST

--- goat/GoatAST.hs
+++ goat/GoatAST.hs
@@ -1,5 +1,20 @@
 module GoatAST where
 
+-------------------------------- Documentation --------------------------------
+
+-- Authors:
+--   Shizhe Cai (shizhec) - 798125
+--   Weizhuo Zhang (weizhuoz) - 1018329
+--   Mingyang Zhang (mingyangz) - 650242
+--   An Luo (aluo1) - 657605
+
+-- This file contains the abstract syntax tree for the Goat language.
+
+-- The aim of the project is to implement a compiler for a procedural (C-like)
+-- language called Goat.
+
+-------------------------------- Documentation --------------------------------
+
 -------------------------------------------------------------------------------
 -- Identifier.
 -------------------------------------------------------------------------------
