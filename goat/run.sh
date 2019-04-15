fe6ee6dea620e9b6cd6e7d8e81862e167cd310ec
An (Martin) Luo <aluo1.developer@gmail.com>
2019-04-15 18:19:49 +1000

    add test case for multiple main proc

--- goat/run.sh
+++ goat/run.sh
@@ -16,3 +16,8 @@ echo "---------------------- matrix test case ----------------------"
 # testing for matrix
 ./Goat -p testCase/matrix_whitespace.gt > testCase/matrix_whitespace_output.txt
 diff testCase/matrix_whitespace_expecting_output.txt testCase/matrix_whitespace_output.txt
+
+echo "---------------------- multiple main proc checking ----------------------"
+# testing for multiple main procedure
+./Goat -p testCase/mainNumChecking.gt > testCase/mainNumChecking_output.txt
+diff testCase/mainNumChecking_expecting_output.txt testCase/mainNumChecking_output.txt
