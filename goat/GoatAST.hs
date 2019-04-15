0bb60ff45df085e0bbbda6c00b4f1218637d8dd5
An (Martin) Luo <aluo1.developer@gmail.com>
2019-04-15 17:58:24 +1000

    remove author information

--- goat/GoatAST.hs
+++ goat/GoatAST.hs
@@ -2,12 +2,6 @@ module GoatAST where
 
 -------------------------------- Documentation --------------------------------
 
--- Authors:
---   Shizhe Cai (shizhec) - 798125
---   Weizhuo Zhang (weizhuoz) - 1018329
---   Mingyang Zhang (mingyangz) - 650242
---   An Luo (aluo1) - 657605
-
 -- This file contains the abstract syntax tree for the Goat language.
 
 -- The aim of the project is to implement a compiler for a procedural (C-like)
--- goat/GoatParser.hs
+++ goat/GoatParser.hs
@@ -9,12 +9,6 @@ import qualified Text.Parsec.Token as Q
 
 -------------------------------- Documentation --------------------------------
 
--- Authors:
---   Shizhe Cai (shizhec) - 798125
---   Weizhuo Zhang (weizhuoz) - 1018329
---   Mingyang Zhang (mingyangz) - 650242
---   An Luo (aluo1) - 657605
-
 -- This file contains the parser-related information of the Goat program.
 
 -- The aim of the project is to implement a compiler for a procedural (C-like)
