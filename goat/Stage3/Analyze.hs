ed0d1a45b9fd2b7724d1c2c052514954acb8ea0e
An (Martin) Luo <aluo1.developer@gmail.com>
2019-05-05 22:12:58 +1000

    add 3 placeholder modules/files

---
+++ goat/Stage3/Analyze.hs
@@ -0,0 +1,16 @@
+module Analyze where
+
+-------------------------------- Documentation --------------------------------
+
+-- Authors:
+--   Shizhe Cai (shizhec) - 798125
+--   Weizhuo Zhang (weizhuoz) - 1018329
+--   Mingyang Zhang (mingyangz) - 650242
+--   An Luo (aluo1) - 657605
+
+-- This file contains the codes that analyze AST's emantics.
+
+-- The aim of the project is to implement a compiler for a procedural (C-like)
+-- language called Goat.
+
+-------------------------------- Documentation --------------------------------
