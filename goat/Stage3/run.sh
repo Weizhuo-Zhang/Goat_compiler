30e3d3576d177bddc48170bd90eddb49783c3d5b
An (Martin) Luo <aluo1.developer@gmail.com>
2019-05-25 20:37:47 +1000

    add new test case, update run shell script

--- goat/Stage3/run.sh
+++ goat/Stage3/run.sh
@@ -10,7 +10,10 @@ cd ../goat/Stage3
 
 echo "\n\n===================== Stage 3 ======================\n"
 
-echo "---------------------- hello.gt ----------------------"
+echo "--------------------- hello.gt ---------------------"
 ./Goat testCase/hello.gt > ./testCase/GeneratedOutput/hello.oz
 diff ./testCase/GeneratedOutput/hello.oz ./testCase/ExpectedOutput/hello.oz
 ./oz ./testCase/GeneratedOutput/hello.oz
+
+echo "----------- asgWithNumericOperatorOnly.gt ----------"
+./Goat -s ./testCase/asgWithNumericOperatorOnly.gt
