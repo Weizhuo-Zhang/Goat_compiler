671a45be636b862f7e7d3303adbe37f9ca06a6c3
An (Martin) Luo <aluo1.developer@gmail.com>
2019-05-28 22:19:06 +1000

    Add peers test case in

--- goat/Stage3/run.sh
+++ goat/Stage3/run.sh
@@ -172,4 +172,189 @@ echo "------------------------- stddev.gt -------------------------"
 
 echo "------------------ End of Visible folder ------------------\n"
 
+echo "-------------------- Visible folder... --------------------\n"
+echo "------------------------- aadornobenit.gt -------------------------"
+./Goat ./testCase/peers/aadornobenit.gt > ./testCase/GeneratedOutput/peers/aadornobenit.oz
+./oz ./testCase/GeneratedOutput/peers/aadornobenit.oz
+
+echo "------------------------- alanu.gt -------------------------"
+./Goat ./testCase/peers/alanu.gt > ./testCase/GeneratedOutput/peers/alanu.oz
+./oz ./testCase/GeneratedOutput/peers/alanu.oz
+
+echo "------------------------- aluo1.gt -------------------------"
+./Goat ./testCase/peers/aluo1.gt > ./testCase/GeneratedOutput/peers/aluo1.oz
+./oz ./testCase/GeneratedOutput/peers/aluo1.oz
+
+echo "------------------------- apurushotha.gt -------------------------"
+./Goat ./testCase/peers/apurushotha.gt > ./testCase/GeneratedOutput/peers/apurushotha.oz
+./oz ./testCase/GeneratedOutput/peers/apurushotha.oz
+
+echo "------------------------- austinl.bad.gt -------------------------"
+./Goat ./testCase/peers/austinl.bad.gt > ./testCase/GeneratedOutput/peers/austinl.bad.oz
+./oz ./testCase/GeneratedOutput/peers/austinl.bad.oz
+
+echo "------------------------- bgooding.gt -------------------------"
+./Goat ./testCase/peers/bgooding.gt > ./testCase/GeneratedOutput/peers/bgooding.oz
+./oz ./testCase/GeneratedOutput/peers/bgooding.oz
+
+echo "------------------------- chenqinz.gt -------------------------"
+./Goat ./testCase/peers/chenqinz.gt > ./testCase/GeneratedOutput/peers/chenqinz.oz
+./oz ./testCase/GeneratedOutput/peers/chenqinz.oz
+
+echo "------------------------- chunyaow.gt -------------------------"
+./Goat ./testCase/peers/chunyaow.gt > ./testCase/GeneratedOutput/peers/chunyaow.oz
+./oz ./testCase/GeneratedOutput/peers/chunyaow.oz
+
+echo "------------------------- codya.gt -------------------------"
+./Goat ./testCase/peers/codya.gt > ./testCase/GeneratedOutput/peers/codya.oz
+./oz ./testCase/GeneratedOutput/peers/codya.oz
+
+echo "------------------------- dbarrell.gt -------------------------"
+./Goat ./testCase/peers/dbarrell.gt > ./testCase/GeneratedOutput/peers/dbarrell.oz
+./oz ./testCase/GeneratedOutput/peers/dbarrell.oz
+
+echo "------------------------- dstern.gt -------------------------"
+./Goat ./testCase/peers/dstern.gt > ./testCase/GeneratedOutput/peers/dstern.oz
+./oz ./testCase/GeneratedOutput/peers/dstern.oz
+
+echo "------------------------- farrugiam.gt -------------------------"
+./Goat ./testCase/peers/farrugiam.gt > ./testCase/GeneratedOutput/peers/farrugiam.oz
+./oz ./testCase/GeneratedOutput/peers/farrugiam.oz
+
+echo "------------------------- glaw.gt -------------------------"
+./Goat ./testCase/peers/glaw.gt > ./testCase/GeneratedOutput/peers/glaw.oz
+./oz ./testCase/GeneratedOutput/peers/glaw.oz
+
+echo "------------------------- guoenj.gt -------------------------"
+./Goat ./testCase/peers/guoenj.gt > ./testCase/GeneratedOutput/peers/guoenj.oz
+./oz ./testCase/GeneratedOutput/peers/guoenj.oz
+
+echo "------------------------- haohail.runbad.gt -------------------------"
+./Goat ./testCase/peers/haohail.runbad.gt > ./testCase/GeneratedOutput/peers/haohail.runbad.oz
+./oz ./testCase/GeneratedOutput/peers/haohail.runbad.oz
+
+echo "------------------------- harald.gt -------------------------"
+./Goat ./testCase/peers/harald.gt > ./testCase/GeneratedOutput/peers/harald.oz
+./oz ./testCase/GeneratedOutput/peers/harald.oz
+
+echo "------------------------- jiaxingt.gt -------------------------"
+./Goat ./testCase/peers/jiaxingt.gt > ./testCase/GeneratedOutput/peers/jiaxingt.oz
+./oz ./testCase/GeneratedOutput/peers/jiaxingt.oz
+
+echo "------------------------- liangy6.gt -------------------------"
+./Goat ./testCase/peers/liangy6.gt > ./testCase/GeneratedOutput/peers/liangy6.oz
+./oz ./testCase/GeneratedOutput/peers/liangy6.oz
+
+echo "------------------------- linghanz1.gt -------------------------"
+./Goat ./testCase/peers/linghanz1.gt > ./testCase/GeneratedOutput/peers/linghanz1.oz
+./oz ./testCase/GeneratedOutput/peers/linghanz1.oz
+
+echo "------------------------- liyaoz.gt -------------------------"
+./Goat ./testCase/peers/liyaoz.gt > ./testCase/GeneratedOutput/peers/liyaoz.oz
+./oz ./testCase/GeneratedOutput/peers/liyaoz.oz
+
+echo "------------------------- lpendock.gt -------------------------"
+./Goat ./testCase/peers/lpendock.gt > ./testCase/GeneratedOutput/peers/lpendock.oz
+./oz ./testCase/GeneratedOutput/peers/lpendock.oz
+
+echo "------------------------- mchan.gt -------------------------"
+./Goat ./testCase/peers/mchan.gt > ./testCase/GeneratedOutput/peers/mchan.oz
+./oz ./testCase/GeneratedOutput/peers/mchan.oz
+
+echo "------------------------- mingdam.gt -------------------------"
+./Goat ./testCase/peers/mingdam.gt > ./testCase/GeneratedOutput/peers/mingdam.oz
+./oz ./testCase/GeneratedOutput/peers/mingdam.oz
+
+echo "------------------------- mingyangz.gt -------------------------"
+./Goat ./testCase/peers/mingyangz.gt > ./testCase/GeneratedOutput/peers/mingyangz.oz
+./oz ./testCase/GeneratedOutput/peers/mingyangz.oz
+
+echo "------------------------- minjianc.gt -------------------------"
+./Goat ./testCase/peers/minjianc.gt > ./testCase/GeneratedOutput/peers/minjianc.oz
+./oz ./testCase/GeneratedOutput/peers/minjianc.oz
+
+echo "------------------------- peiyuh.gt -------------------------"
+./Goat ./testCase/peers/peiyuh.gt > ./testCase/GeneratedOutput/peers/peiyuh.oz
+./oz ./testCase/GeneratedOutput/peers/peiyuh.oz
+
+echo "------------------------- raymonds1.gt -------------------------"
+./Goat ./testCase/peers/raymonds1.gt > ./testCase/GeneratedOutput/peers/raymonds1.oz
+./oz ./testCase/GeneratedOutput/peers/raymonds1.oz
+
+echo "------------------------- shijiel2.gt -------------------------"
+./Goat ./testCase/peers/shijiel2.gt > ./testCase/GeneratedOutput/peers/shijiel2.oz
+./oz ./testCase/GeneratedOutput/peers/shijiel2.oz
+
+echo "------------------------- shizhec.gt -------------------------"
+./Goat ./testCase/peers/shizhec.gt > ./testCase/GeneratedOutput/peers/shizhec.oz
+./oz ./testCase/GeneratedOutput/peers/shizhec.oz
+
+echo "------------------------- shumaox.gt -------------------------"
+./Goat ./testCase/peers/shumaox.gt > ./testCase/GeneratedOutput/peers/shumaox.oz
+./oz ./testCase/GeneratedOutput/peers/shumaox.oz
+
+echo "------------------------- stevent2.gt -------------------------"
+./Goat ./testCase/peers/stevent2.gt > ./testCase/GeneratedOutput/peers/stevent2.oz
+./oz ./testCase/GeneratedOutput/peers/stevent2.oz
+
+echo "------------------------- weizhix.gt -------------------------"
+./Goat ./testCase/peers/weizhix.gt > ./testCase/GeneratedOutput/peers/weizhix.oz
+./oz ./testCase/GeneratedOutput/peers/weizhix.oz
+
+echo "------------------------- weizhuoz.gt -------------------------"
+./Goat ./testCase/peers/weizhuoz.gt > ./testCase/GeneratedOutput/peers/weizhuoz.oz
+./oz ./testCase/GeneratedOutput/peers/weizhuoz.oz
+
+echo "------------------------- wenqingx.gt -------------------------"
+./Goat ./testCase/peers/wenqingx.gt > ./testCase/GeneratedOutput/peers/wenqingx.oz
+./oz ./testCase/GeneratedOutput/peers/wenqingx.oz
+
+echo "------------------------- wentaol4.gt -------------------------"
+./Goat ./testCase/peers/wentaol4.gt > ./testCase/GeneratedOutput/peers/wentaol4.oz
+./oz ./testCase/GeneratedOutput/peers/wentaol4.oz
+
+echo "------------------------- wleong3.gt -------------------------"
+./Goat ./testCase/peers/wleong3.gt > ./testCase/GeneratedOutput/peers/wleong3.oz
+./oz ./testCase/GeneratedOutput/peers/wleong3.oz
+
+echo "------------------------- yigew3.gt -------------------------"
+./Goat ./testCase/peers/yigew3.gt > ./testCase/GeneratedOutput/peers/yigew3.oz
+./oz ./testCase/GeneratedOutput/peers/yigew3.oz
+
+echo "------------------------- yiqunw1.bad.gt -------------------------"
+./Goat ./testCase/peers/yiqunw1.bad.gt > ./testCase/GeneratedOutput/peers/yiqunw1.bad.oz
+./oz ./testCase/GeneratedOutput/peers/yiqunw1.bad.oz
+
+echo "------------------------- yixiong.gt -------------------------"
+./Goat ./testCase/peers/yixiong.gt > ./testCase/GeneratedOutput/peers/yixiong.oz
+./oz ./testCase/GeneratedOutput/peers/yixiong.oz
+
+echo "------------------------- yiyue.gt -------------------------"
+./Goat ./testCase/peers/yiyue.gt > ./testCase/GeneratedOutput/peers/yiyue.oz
+./oz ./testCase/GeneratedOutput/peers/yiyue.oz
+
+echo "------------------------- yuankail.gt -------------------------"
+./Goat ./testCase/peers/yuankail.gt > ./testCase/GeneratedOutput/peers/yuankail.oz
+./oz ./testCase/GeneratedOutput/peers/yuankail.oz
+
+echo "------------------------- zeleic.gt -------------------------"
+./Goat ./testCase/peers/zeleic.gt > ./testCase/GeneratedOutput/peers/zeleic.oz
+./oz ./testCase/GeneratedOutput/peers/zeleic.oz
+
+echo "------------------------- zeyuh3.gt -------------------------"
+./Goat ./testCase/peers/zeyuh3.gt > ./testCase/GeneratedOutput/peers/zeyuh3.oz
+./oz ./testCase/GeneratedOutput/peers/zeyuh3.oz
+
+echo "------------------------- zhengjiex1.gt -------------------------"
+./Goat ./testCase/peers/zhengjiex1.gt > ./testCase/GeneratedOutput/peers/zhengjiex1.oz
+./oz ./testCase/GeneratedOutput/peers/zhengjiex1.oz
+
+echo "------------------------- zhet1.gt -------------------------"
+./Goat ./testCase/peers/zhet1.gt > ./testCase/GeneratedOutput/peers/zhet1.oz
+./oz ./testCase/GeneratedOutput/peers/zhet1.oz
+
+echo "------------------------- zijunc3.gt -------------------------"
+./Goat ./testCase/peers/zijunc3.gt > ./testCase/GeneratedOutput/peers/zijunc3.oz
+./oz ./testCase/GeneratedOutput/peers/zijunc3.oz
+
 make clean
